98d4e4b4464d9daefa0eddde6be46db401f9abae
maxwellmattryan <maxwellmattryan@gmail.com>
2020-10-24 23:33:00 -0500

    Fix some colors in the stylings

--- ui/src/app/modules/home/home.component.ts
+++ ui/src/app/modules/home/home.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
-    isLoaded: boolean = false;
+    isLoaded: boolean = true;
 
     constructor() { }
 
