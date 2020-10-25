5369e6de6182adf7315d1b1c14e7007a2be9f4f8
maxwellmattryan <maxwellmattryan@gmail.com>
2020-10-24 21:46:21 -0500

    Add node type dependency and update routing

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
-import { environment } from '../environments/environment';
+import { environment } from '@ui/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { HomeModule } from '@ui/modules';
 
 @NgModule({
     declarations: [
@@ -14,6 +16,7 @@ import { AppComponent } from './app.component';
     imports: [
         BrowserModule,
         AppRoutingModule,
+        HomeModule,
         ServiceWorkerModule.register('ngsw-worker.js', {
             enabled: environment.production,
             registrationStrategy: 'registerImmediately'
