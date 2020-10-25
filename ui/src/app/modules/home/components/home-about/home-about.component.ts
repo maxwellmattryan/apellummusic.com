46da55e3bdfb1bf9ec2a22736613790e02db3922
maxwellmattryan <maxwellmattryan@gmail.com>
2020-10-25 00:28:59 -0500

    Add svg icons for spotify and youtube + fix styling

--- ui/src/app/modules/home/components/home-about/home-about.component.ts
+++ ui/src/app/modules/home/components/home-about/home-about.component.ts
@@ -9,7 +9,7 @@ export class HomeAboutComponent implements OnInit {
     @Input() paragraph: string = `
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec metus lectus, vestibulum in lobortis in, consequat et lacus. Curabitur iaculis bibendum sem id dapibus. Duis tempus orci ut leo ornare varius. Ut mollis posuere sapien fringilla gravida. Sed in lorem rutrum, tincidunt purus vel, volutpat massa. Curabitur lobortis convallis tortor, nec scelerisque nibh consectetur eu. Maecenas gravida metus nec nunc fringilla, sed consequat nibh venenatis.
         Pellentesque et convallis justo. Vestibulum quis pulvinar magna. Cras id neque pellentesque est placerat convallis. Curabitur euismod diam quam, a aliquam dolor suscipit eget. Ut vitae urna est. Etiam libero dui, pretium non purus id, hendrerit rhoncus ligula. Ut placerat leo in metus lobortis lobortis. Sed nisl massa, sollicitudin sit amet risus nec, aliquet gravida velit.`;
-    @Input() imageUrl: string = 'https://mattmaxwell.dev/assets/images/portrait.png';
+    @Input() imageUrl: string = 'https://mattmaxwell.dev/assets/images/portfolio/profiles/portrait.png';
 
     constructor() { }
 
