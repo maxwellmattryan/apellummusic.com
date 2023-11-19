861f76e69c9bd4f564412be23dd846737bb459ef
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-11-19 16:13:44 -0600

    Setup project files more

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
-	import '../app.css';
+	import '../app.css'
 </script>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+</nav>
 
 <slot />
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte'
+	import { Hello } from '@components'
+	import { hello } from '@lib'
+
+	onMount(() => {
+		hello()
+	})
+</script>
+
+<Hello />
