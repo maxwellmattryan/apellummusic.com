6067070870c4c6411ae4a1ac535562f124e08790
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-11-26 12:27:22 -0600

    Use different asset path

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import { base } from '$app/paths'
 	import { onMount } from 'svelte'
 	import { AppRoute, setAppRoute } from '@lib/app'
+
+	const backgroundImageUrl = `${base}/images/photo_press_home.jpg`
 
 	onMount(() => {
 		setAppRoute(AppRoute.Home)
@@ -12,18 +15,19 @@
 		<div
 			class="w-full max-w-3xl h-full px-[6rem] flex flex-col justify-center items-end text-right"
 		>
-			<img src="./logos/white.svg" alt="Apellum logo" />
+			<img src="{base}/logos/white.svg" alt="Apellum logo" />
 			<!-- ghost header -->
 			<h1 class="invisible h-0">Apellum</h1>
 			<h2 class="mt-4 italic text-right text-[#eeeff9] opacity-90">Techno producer & DJ</h2>
 		</div>
 	</home-page-left>
-	<home-page-right class="w-1/2 h-full"> </home-page-right>
+	<home-page-right class="w-1/2 h-full" style="--bg-image-url: url({backgroundImageUrl})">
+	</home-page-right>
 </home-page>
 
 <style lang="postcss">
 	home-page-right {
-		background-image: url('./images/photo_press_home.jpg');
+		background-image: var(--bg-image-url);
 		@apply bg-cover bg-center;
 	}
 </style>
