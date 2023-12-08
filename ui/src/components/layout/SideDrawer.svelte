04df79524b3ef3a2c05958de3f6cf1fd2f5a8750
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-12-08 15:20:56 -0600

    Style website nav in side drawer

--- ui/src/components/layout/SideDrawer.svelte
+++ ui/src/components/layout/SideDrawer.svelte
@@ -6,12 +6,13 @@
 <side-drawer
 	class="absolute top-0 right-0 {$sideDrawerState.hidden
 		? 'w-0'
-		: 'w-screen'} h-screen md:invisible z-20 overflow-x-hidden"
+		: 'w-screen'} h-screen md:invisible z-20 overflow-x-hidden flex flex-col items-end gap-[5vh]"
 >
-	<button type="button" on:click={closeSideDrawer}>
+	<button type="button" on:click={closeSideDrawer} class="mt-6 mr-6">
 		<Icon icon={IconName.Close} />
 	</button>
-	<WebsiteNavbar />
+	<WebsiteNavbar context="drawer" direction="right" />
+	<!--	<LinkSidebar />-->
 </side-drawer>
 
 <style lang="postcss">
@@ -20,5 +21,9 @@
 
 		transition: width 500ms ease-in-out;
 		animation-fill-mode: both;
+	}
+
+	:global(a) {
+		@apply text-lg !important;
 	}
 </style>
--- ui/src/components/layout/WebsiteNavbar.svelte
+++ ui/src/components/layout/WebsiteNavbar.svelte
@@ -2,6 +2,9 @@
 	import { appRoute, AppRoute, closeSideDrawer, getPageHeader, setAppRoute } from '@lib/app'
 	import { FEATURES } from '@lib/features'
 
+	export let context: 'page' | 'drawer' = 'page'
+	export let direction: 'left' | 'right' = 'left'
+
 	function getAppRoutes(): AppRoute[] {
 		return Object.values(AppRoute).filter((route) => FEATURES[route]?.enabled)
 	}
@@ -42,26 +45,46 @@
 	}
 </script>
 
-<website-navbar class="absolute top-[8vh] left-4">
+<website-navbar class="{context} {direction}-4">
 	<nav class="flex flex-col">
 		{#each getAppRoutes() as route}
 			{@const selected = route === $appRoute}
 			<website-navbar-link
 				class:selected
 				class:invisible={FEATURES[route]?.hidden}
-				class="group h-[1.8125rem]"
+				class="group h-[{context === 'page' ? '1.8125' : '3'}rem] {direction === 'right'
+					? 'text-right'
+					: ''}"
 			>
-				<website-navbar-link-highlight class="border-solid border-l-2"
-				></website-navbar-link-highlight>
-				<a href={'/' + getAppRouteUrl(route)} on:click={() => onAppRouteClick(route)}
-					>{getPageHeader(route)}</a
+				{#if direction === 'left'}
+					<website-navbar-link-highlight class="border-solid border-l-2"
+					></website-navbar-link-highlight>
+				{/if}
+				<a
+					href={'/' + getAppRouteUrl(route)}
+					on:click={() => onAppRouteClick(route)}
+					class={direction === 'left' ? 'ml-4' : 'mr-4'}>{getPageHeader(route)}</a
 				>
+				{#if direction === 'right'}
+					<website-navbar-link-highlight class="border-solid border-r-2"
+					></website-navbar-link-highlight>
+				{/if}
 			</website-navbar-link>
 		{/each}
 	</nav>
 </website-navbar>
 
 <style lang="postcss">
+	website-navbar {
+		&.page {
+			@apply absolute top-[8vh];
+		}
+
+		&.drawer {
+			@apply mt-[8vh] pr-8;
+		}
+	}
+
 	website-navbar-link {
 		@apply opacity-60;
 		@apply transition-opacity;
@@ -81,9 +104,6 @@
 
 		& website-navbar-link-highlight {
 			@apply border-slate-950;
-		}
-		& a {
-			@apply ml-4;
 		}
 	}
 </style>
