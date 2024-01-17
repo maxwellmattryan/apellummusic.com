fce7edd65a466814301021b446c66a54efc579f8
Matthew Maxwell <maxwellmattryan@gmail.com>
2024-01-16 22:49:52 -0600

    chore: create music section component

--- ui/src/routes/music/+page.svelte
+++ ui/src/routes/music/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import { onMount } from 'svelte'
 	import { AppRoute, getPageTitle, setAppRoute } from '@lib/app'
+	import { MUSIC_DATA } from '@lib/data/music'
+	import { MusicSection } from './components'
+
+	const { collections } = MUSIC_DATA
 
 	onMount(() => {
 		setAppRoute(AppRoute.Music)
@@ -10,6 +14,8 @@
 <svelte:head>
 	<title>{getPageTitle(AppRoute.Music)}</title>
 </svelte:head>
-<music-page class="w-full h-[50vh] flex justify-center items-center">
-	<p>Coming soon ...</p>
+<music-page class="w-full h-auto mt-[10vh] mb-[15vh] flex flex-col items-center">
+	{#each Object.entries(collections) as [collectionType, collection]}
+		<MusicSection {collectionType} {collection} />
+	{/each}
 </music-page>
