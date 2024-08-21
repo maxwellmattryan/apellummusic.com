abd375f35c7015374373a66de941370b45a5dc18
Matthew Maxwell <maxwellmattryan@gmail.com>
2024-08-21 10:05:12 -0500

    Update bio

--- ui/src/lib/data/epk/epk-data.constant.ts
+++ ui/src/lib/data/epk/epk-data.constant.ts
@@ -6,6 +6,16 @@ export const EPK_DATA: IEpkData = {
 	downloadUrl: 'https://storage.googleapis.com/apellum-files/epks/apellum-epk.zip',
 	biographyData: {
 		image: 'press_epk-01.jpg',
+		/**
+		 * When updating the biography, make sure to make changes in the following places:
+		 * - Bandcamp
+		 * - Beatport
+		 * - Google Drive (upload to GCP)
+		 * - Kinetic Management
+		 * - Resident Advisor
+		 * - Spotify
+		 * - Website (source of truth)
+		 */
 		biographies: {
 			[EpkBiographyType.OneLine]: {
 				type: EpkBiographyType.OneLine,
@@ -19,7 +29,7 @@ export const EPK_DATA: IEpkData = {
 				type: EpkBiographyType.Short,
 				text: [
 					`
-					In the dynamic landscape of electronic music, Apellum stands as a techno producer and DJ deeply rooted in the vibrant Austin scene. With a diverse background as a percussionist, audio engineer, and sound designer, Apellum artfully weaves together influences from a vast spectrum of musical genres, crafting a sonic tapestry that resonates closely with the groovy, hypnotic, and raw essence of Berlin techno. Fans can look forward to his new EP, "Scenes of Water", which is an experience exploring the natural world as it features water, represented by the dub sounds in the music. 
+					In the dynamic landscape of electronic music, Apellum stands as a techno producer and DJ deeply rooted in the vibrant Austin scene. With a diverse background as a percussionist, audio engineer, and sound designer, Apellum artfully weaves together influences from a vast spectrum of musical genres, crafting a sonic tapestry that resonates closely with the groovy, hypnotic, and raw essence of Berlin techno. Fans can look forward to his new EP, "Voyager", which is an experience exploring the vastness and hostility of space travel in a futuristic world. 
 				`,
 				],
 			},
@@ -30,10 +40,10 @@ export const EPK_DATA: IEpkData = {
 					In the dynamic landscape of electronic music, Apellum stands as a techno producer and DJ deeply rooted in the vibrant Austin scene. With a diverse background as a percussionist, audio engineer, and sound designer, Apellum artfully weaves together influences from a vast spectrum of musical genres, crafting a sonic tapestry that resonates closely with the groovy, hypnotic, and raw essence of Berlin techno.
 				`,
 					`
-					Within the techno scene, Apellum finds inspiration in many artists from multiple sub-genres, such as Ketch or Marcal for raw Berlin techno, Chlär for powerful and groovy techno, or Wata Igarashi for hypnotic dub techno. Beyond the confines of techno, Apellum’s creative canvas is painted with shades of jungle, trance, jazz, fusion, funk, and classical, among many others.
+					Within the techno scene, Apellum finds inspiration in many artists from multiple sub-genres, such as Rene Wise for raw and groovy techno, Ketch for dark and cerebral techno, and Wata Igarashi for hypnotic dub techno. Beyond the confines of techno, Apellum's creative canvas is painted with shades of jungle, trance, jazz, fusion, funk, and classical, among many others.
 				`,
 					`
-					Apellum’s studio has been a realm of exploration and creativity, where new sounds and music are discovered and crafted. Fans can look forward to his new EP, "Scenes of Water", which is an experience exploring the natural world as it features water, represented by the dub sounds in the music.
+					Apellum's studio has been a realm of exploration and creativity, where new sounds and music are discovered and crafted. Fans can look forward to his new EP, "Voyager", which is an experience exploring the vastness and hostility of space travel in a futuristic world.
 				`,
 				],
 			},
